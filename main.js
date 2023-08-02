3b3d1c4d0792efb9afbcc2504c9cae94fa2f769a
EDWAR NEWGATE <136027194+EduardoMoraes2106@users.noreply.github.com>
2023-08-02 20:54:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let Btn = document.getElementById('btn')
+
+Btn.addEventListener('click', ()=>{
+    alert('PORQUE VOCE APERTOU ESSE BOTAO?')
+
+    location.href = 'https://youtu.be/fZmAC-xt9qc'
+})
